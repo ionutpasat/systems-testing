be4a73af7f020132ebcc03d0042617955b4a5ddc
Ionut Pasat <ionut_vladut.pasat@stud.acs.upb.ro>
2025-04-28 01:02:31 +0300

    added unit tests and action for running tests on PR

---
+++ laboratories/cicd-documentation/test_tree.py
@@ -0,0 +1,22 @@
+import unittest
+from tree import Tree
+from node import Node
+
+class TestTree(unittest.TestCase):
+
+    def setUp(self):
+        self.tree = Tree()
+        for value in [5, 3, 8, 1, 4]:
+            self.tree.add(value)
+
+    def test_find_existing_node(self):
+        node = self.tree.find(3)
+        self.assertIsNotNone(node)
+        self.assertEqual(node.data, 3)
+
+    def test_find_non_existing_node(self):
+        node = self.tree.find(10)
+        self.assertIsNone(node)
+
+if __name__ == '__main__':
+    unittest.main()
